29a56a337713e2c5dfe71f806243200da51611f2
Nageswar <nagesh23479@gmail.com>
2026-02-12 09:15:34 +0530

    test

--- 03-while-loop2.sh
+++ 03-while-loop2.sh
@@ -7,7 +7,7 @@ USAGE_THRESHOLD=3
 while IFS= read -r $line
 do
 USAGE=$( echo $line | awk '{print $6}' | cut -d "%" -f1 )
-PARTITION=$(echo $line | awk '{print $7}')
+PARTITION=$( echo $line | awk '{print $7}' )
 if [ "$USAGE" -ge "$USAGE_THRESHOLD" ]; then
     MESSAGE+="High disk utilization on $PARTITION; $USAGE%"
 fi
