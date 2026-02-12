d6367ba74d317032253a125270a17203d4b89069
Nageswar <nagesh23479@gmail.com>
2026-02-12 07:43:45 +0530

    test

--- 01-while-loop.sh
+++ 01-while-loop.sh
@@ -5,12 +5,11 @@ USAGE_THRESHOLD=3
 
 while IFS= read -r line
 do 
-    USAGE=$(echo $line | awk '{print $6}' | cut -d "%' -f1)
+    USAGE=$(echo $line | awk '{print $6}' | cut -d "%" -f1)
     PARTITION=$(echo $line | awk '{print $7}')
 
     if [ "$USAGE" -ge "$USAGE_THRESHOLD" ]; then
         MESSAGE+="Higi disk usage on $PARTITION: $USAGE% <br>"
     fi
 done <<< $DISK_USAGE
-
 echo -e "$MESSAGE"
