6f2c2fc7556c9822715463fa1c34a5f11a93fd05
Nageswar <nagesh23479@gmail.com>
2026-02-12 08:02:15 +0530

    test

--- 02-while-loop1.sh
+++ 02-while-loop1.sh
@@ -6,7 +6,7 @@ USAGE_THRESHOLD=3
 
 while IFS= read -r line 
 do 
-    USAGE=$( echo $line | awk '{print $6}' | cut -d "%" )
+    USAGE=$( echo $line | awk '{print $6}' | cut -d "%" -f1 )
     PARTITION=$( echo $line | awk '{print $7}')
 
     if [ "$USAGE" -ge "$USAGE_THRESHOLD" ]; then
